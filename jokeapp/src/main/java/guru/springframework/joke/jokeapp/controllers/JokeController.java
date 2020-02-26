995d8651c733cffff1b7e691a218e0234b50c78d
Payton Mitchell <payton016@gmail.com>
2020-02-25 21:16:31 -0500

    service

---
+++ jokeapp/src/main/java/guru/springframework/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,5 @@
+package guru.springframework.joke.jokeapp.controllers;
+
+public class JokeController {
+
+}
---
+++ jokeapp/src/main/java/guru/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.joke.jokeapp.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+	
+	
+	
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+
+
+	@Override
+	public String getJoke() {
+		// TODO Auto-generated method stub
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+	
+	
+
+}
